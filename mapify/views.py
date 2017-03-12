39758afe0079d7c5b53ffc811b5dda9d4156bd62
abhishek541 <ak6210@nyu.edu>
2017-03-12 15:44:02 -0400

    Changed view for scroll query

--- mapify/views.py
+++ mapify/views.py
@@ -25,22 +25,43 @@ es = Elasticsearch(['https://search-mapify-tweets-pcgkfb5ccbz3zi2ptcnkmsiqfi.us-
 #Get a sample of the public data following through Twitter
 iterator = twitter_stream.statuses.sample()
 
-tweet_count = 1000
-for tweet in iterator:
-    tweet_count -= 1
-    tw = json.dumps(tweet)
-    try:
-        es.index(index="idx_twp", doc_type="twitter_twp", id=tw['id'], body=tw)
-    except Exception:
-        pass
-    #print (type(tw))
-    if tweet_count <= 0:
-        break
+# tweet_count = 100
+# for tweet in iterator:
+#     tweet_count -= 1
+#     tw = json.dumps(tweet)
+#     try:
+#         es.index(index="idx_twp", doc_type="twitter_twp", id=1, body=tw)
+#     except Exception:
+#         pass
+#     print (tw)
+#     if tweet_count <= 0:
+#         break
 
 # print(res)
 
+# res = es.search(index="idx_twp", doc_type="twitter_twp", q='text:"a"')
+# print(res)
+
 def getTweets(request):
-    keyword = request.POST('keyword')
-    res = es.search(index="idx_twp", doc_type="twitter_twp", q='text:"'+keyword+'"')
-    tweetJson = json.dumps(res)
-    return render(request, 'mapify/index.html', {'tweetJson': tweetJson})
+    try:
+        keyword = request.POST['keyword']
+    except:
+        keyword = ""
+    print("keyword: "+keyword)
+    res = es.search(index="idx_twp", doc_type="twitter_twp", q='text:"'+keyword+'"', scroll='60s', search_type='query_then_fetch')
+    scroll_size = res['hits']['total']
+    tweetJson = []
+    while (scroll_size > 0):
+        try:
+            scroll_id = res['_scroll_id']
+            res = es.scroll(scroll_id=scroll_id, scroll='60s')
+            tweetJson += res['hits']['hits']
+            scroll_size = len(res['hits']['hits'])
+        except:
+            break
+    coordinatesList = []
+    for r in tweetJson:
+        if (r['_source']['coordinates']):
+            coordinatesList.append(r['_source']['coordinates'])
+    #print("result: "+json.dumps(coordinatesList))
+    return render(request, 'mapify/index.html', {'tweetCoord': json.dumps(coordinatesList)})
