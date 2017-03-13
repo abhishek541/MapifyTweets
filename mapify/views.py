2e2c3fd55789e89ceba332484c78db755a3bd72d
abhishek541 <ak6210@nyu.edu>
2017-03-12 22:25:36 -0400

    Changed views for elastic beanstalk

--- mapify/views.py
+++ mapify/views.py
@@ -8,39 +8,9 @@ try:
 except ImportError:
     import simplejson as json
 
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from elasticsearch import Elasticsearch
 
-CONSUMER_KEY="jw6AtHQRo9gCcaQs6OzTzeNRT"
-CONSUMER_SECRET="8oTz45vr3E63O0CP0bqmJf1vCVB8cwrwazRx4uh5jfbnjCM55p"
-
-ACCESS_TOKEN="158373112-Yh9wdWSMxEXVAMAqJFabDbezzDucgctnRaoIx3nb"
-ACCESS_SECRET="HMd4PNldTgLxcukctoc2Jp8DMtibxDGa7LhtzxFf7kS5I"
-
-oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-
-twitter_stream = TwitterStream(auth=oauth)
-
 es = Elasticsearch(['https://search-mapify-tweets-pcgkfb5ccbz3zi2ptcnkmsiqfi.us-east-1.es.amazonaws.com/'])
-#Get a sample of the public data following through Twitter
-iterator = twitter_stream.statuses.sample()
-
-# tweet_count = 100
-# for tweet in iterator:
-#     tweet_count -= 1
-#     tw = json.dumps(tweet)
-#     try:
-#         es.index(index="idx_twp", doc_type="twitter_twp", id=1, body=tw)
-#     except Exception:
-#         pass
-#     print (tw)
-#     if tweet_count <= 0:
-#         break
-
-# print(res)
-
-# res = es.search(index="idx_twp", doc_type="twitter_twp", q='text:"a"')
-# print(res)
 
 def getTweets(request):
     try:
