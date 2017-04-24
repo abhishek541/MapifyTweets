14cb04cbca578c220f68eb213793c791d58389c6
abhishek541 <ak6210@nyu.edu>
2017-04-23 21:26:15 -0400

    Modified urls

--- MapifyTweets/urls.py
+++ MapifyTweets/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'/', include('mapify.urls')),
+    url(r'^', include('mapify.urls')),
     url(r'^admin/', admin.site.urls),
 ]
