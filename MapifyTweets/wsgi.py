4b3f2f9682363219de48cab146f7e27e3b4f7211
abhishek541 <ak6210@nyu.edu>
2017-04-23 23:34:06 -0400

    Modified wsgi.py

--- MapifyTweets/wsgi.py
+++ MapifyTweets/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/opt/python/current/app/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MapifyTweets.settings")
 
 application = get_wsgi_application()
