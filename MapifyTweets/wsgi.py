c264e73f65218b067415b25aa0dbf4ff7b2d8c32
abhishek541 <ak6210@nyu.edu>
2017-04-24 23:33:17 -0400

    Removed config to reset app

--- MapifyTweets/wsgi.py
+++ MapifyTweets/wsgi.py
@@ -7,15 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os, sys
-sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+import os
 
 for k in sorted(os.environ.keys()):
     v = os.environ[k]
     print ('%-30s %s' % (k,v[:70]))
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MapifyTweets.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
